95455c5c5ebfc5b5888666453d7dd3c516b37e63
Andjelka Mladenovic <[andjelkamladenovic8@gmail.com]>
2020-08-17 20:17:09 +0200

    All files added

---
+++ courseVue/course-vue/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  // import InstructorApp from './components/InstructorApp.vue'
+
+  export default {
+    name: 'In28Minutes',
+    // components: {
+    //   InstructorApp
+    // }
+  }
+</script>
+
+<style>
+  @import url(https://unpkg.com/dist/css/bootstrap.min.css);
+</style>
